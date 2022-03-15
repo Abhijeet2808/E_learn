94ec87fc7959bcc5229bc6a8f9aed25a4da69399
Abhijeet <abhijeetmehetre2808@gmail.com>
2022-03-16 02:08:16 +0530

    course complete

--- LearnHub/src/app/course/course.component.ts
+++ LearnHub/src/app/course/course.component.ts
@@ -11,8 +11,10 @@ export class CourseComponent implements OnInit {
 
   ngOnInit(): void {
   }course=[
-    {'id':1,'name':'Learn angular','description':'Angular Full Course - Learn Angular In 3 Hours | Angular Tutorial For Beginners','image':'../../assets/ang.png'},
-    {'id':1,'name':'Learn TypeScript','description':'Angular Full Course - Learn Angular In 3 Hours | Angular Tutorial For Beginners','image':'../../assets/ang.png'}
+    {'id':1,'name':'Learn angular','description':'Angular Full Course - Learn Angular In 3 Hours | Angular Tutorial For Beginners','image':'../../assets/ag2.png'},
+    {'id':1,'name':'Learn TypeScript','description':'TypeScript Full Course - Learn TypeScript In 2 Hours | TypeScript Tutorial For Beginners','image':'../../assets/ts.png'},
+    {'id':1,'name':'Learn Nodejs','description':'Nodejs Full Course - Learn Nodejs In 5 Hours | Nodejs Tutorial For Beginners','image':'../../assets/ns2.png'},
+    {'id':1,'name':'Learn Reactjs','description':'Reactjs Full Course - Learn Reactjs In 3 Hours | Reactjs Tutorial For Beginners','image':'../../assets/rs2.png'}
   
   ]
 }
