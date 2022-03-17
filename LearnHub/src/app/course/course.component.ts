df4aaa16cbb6943d3776fa56ad8e004b46f4d4e1
Abhijeet <abhijeetmehetre2808@gmail.com>
2022-03-17 22:18:14 +0530

    chnges in course

--- LearnHub/src/app/course/course.component.ts
+++ LearnHub/src/app/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course',
@@ -7,14 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
-  }course=[
-    {'id':1,'name':'Learn angular','description':'Angular Full Course - Learn Angular In 3 Hours | Angular Tutorial For Beginners','image':'../../assets/ag2.png'},
-    {'id':1,'name':'Learn TypeScript','description':'TypeScript Full Course - Learn TypeScript In 2 Hours | TypeScript Tutorial For Beginners','image':'../../assets/ts.png'},
-    {'id':1,'name':'Learn Nodejs','description':'Nodejs Full Course - Learn Nodejs In 5 Hours | Nodejs Tutorial For Beginners','image':'../../assets/ns2.png'},
-    {'id':1,'name':'Learn Reactjs','description':'Reactjs Full Course - Learn Reactjs In 3 Hours | Reactjs Tutorial For Beginners','image':'../../assets/rs2.png'}
-  
-  ]
+  }
 }
