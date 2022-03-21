a87812b7bbed5058f7c7b058a797d3e6fe49a421
Abhijeet <abhijeetmehetre2808@gmail.com>
2022-03-21 21:35:41 +0530

    dynamic call

--- LearnHub/src/app/app.module.ts
+++ LearnHub/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +26,8 @@ import { ReadMoreComponent } from './read-more/read-more.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- LearnHub/src/app/course/course.component.ts
+++ LearnHub/src/app/course/course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-course',
@@ -7,9 +8,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  users:any;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,public http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData():void{
+    this.http.get('http://localhost:5000/users').subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    
+    });
+
   }
 }
+
