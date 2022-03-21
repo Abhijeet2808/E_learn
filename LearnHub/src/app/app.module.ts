174a6d42f675e9adcb76a3baf1770b72d1eb381b
Abhijeet <abhijeetmehetre2808@gmail.com>
2022-03-21 16:01:49 +0530

    changes in serve.js

--- LearnHub/src/app/app.module.ts
+++ LearnHub/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import{HttpClientModule} from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +11,6 @@ import { JoinnowComponent } from './joinnow/joinnow.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReadMoreComponent } from './read-more/read-more.component';
-import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +21,11 @@ import { DemoComponent } from './demo/demo.component';
     JoinnowComponent,
     ContactComponent,
     FooterComponent,
-    ReadMoreComponent,
-    DemoComponent
+    ReadMoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
